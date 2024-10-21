c561ce459a29dc85c370a40f5d99a37618ee1d25
Sapegin-RB <sapeginskal@mail.ru>
2024-10-21 23:24:18 +0700

    good code

--- tdpars/tdpars/spiders/tdnewpars.py
+++ tdpars/tdpars/spiders/tdnewpars.py
@@ -1,18 +1,20 @@
 import scrapy
 
-
 class TdnewparsSpider(scrapy.Spider):
     name = "tdnewpars"
-    allowed_domains = ["https://tdom.info"]
+    allowed_domains = ["tdom.info"]
     start_urls = ["https://tdom.info/santehnika/polotencesushiteli-i-komplektuyuschie"]
 
     def parse(self, response):
-        towels = response.css("span.item-name")
+        towels = response.css("div.product-thumb")
         for towel in towels:
+            name = towel.css("span.item-name::text").get()
+            price = towel.css("p.price::text").get()
+            url = towel.css("div.caption a").attrib["href"]
+
+            # Проверяем, что name не None перед вызовом .strip()
             yield {
-                "name" : towel.css("div.caption span.item-name").get(),
-                "price" : towel.css("div.option span.item-name").get(),
-                "url" : towel.css("a").attrib["href"]
+                "name": name.strip() if name else "Название отсутствует",
+                "price": price.strip() if price else "Цена отсутствует",
+                "url": url if url else "URL отсутствует"
             }
-
-
